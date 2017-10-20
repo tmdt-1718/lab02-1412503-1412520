f5d582827ca2cdeeb94c21d2404b7f5cd8568ca8
Nguyễn Hoàng Thi <1412520@student.hcmus.edu.vn>
2017-10-21 01:07:42 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,14 @@ Rails.application.routes.draw do
   post 'session/login', to: 'session#create', as: nil
   delete 'session/logout', to: 'session#destroy', as: :logout
 
-  get  'new_mes', to: 'new_mes#index', as: nil
-  post 'new_mes', to: 'new_mes#create', as: nil
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 	scope '/messages' do
 	  resources :inbox, :sent, only: [:index]
 	  post 'inbox/read', to: 'inbox#read'
+    resources :new_mes, only: [:index, :create]
 	end
-  # resources :messages, only: [:index]
-  resources :user, only: [:new, :create]
 
-  resources :new_mes, only: [:index, :create]
-  # resources :session, only: [:new, :create, :destroy as: nil]
+  resources :user, only: [:new, :create]
 end
