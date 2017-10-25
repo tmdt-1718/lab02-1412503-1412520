f5d739ccc8ad0d8bd7f4ed5c175e68bc157fb446
Nguyễn Hoàng Thi <1412520@student.hcmus.edu.vn>
2017-10-25 22:55:47 +0700

    fix bug

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,13 +4,6 @@ class InboxController < ApplicationController
 	def index
 		@active_inbox = "active"
 		@messages = Inbox.where(recipient_id: current_user[:id]).all.order(created_at: :asc)
-		@messages.each do |message|
-			if current_user.friends.find{|x| x.friend_id == message.message.sender_id} == nil
-				message.isOfFriend = false
-			else
-				message.isOfFriend = true
-			end
-		end
 	end
 
 	def read
--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,5 +1,4 @@
 class Inbox < ApplicationRecord
-	attr_accessor :isOfFriend
 	belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
 	belongs_to :message
 end
